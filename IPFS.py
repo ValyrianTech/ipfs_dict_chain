a0f5b30ef45765ca46a3857d64b3e441ddef5247
Wouter Glorieux <wouter@valyrian.tech>
2023-05-02 00:54:14 +0200

    refactored code

--- IPFS.py
+++ IPFS.py
@@ -6,8 +6,8 @@ import os
 import aioipfs
 from multiaddr import Multiaddr
 from typing import Dict
+from contextlib import contextmanager
 
-IPFS_CACHE = {}
 HOST = '127.0.0.1'
 PORT = 5001
 multi_address = Multiaddr(f'/ip4/{HOST}/tcp/{PORT}')
@@ -16,6 +16,31 @@ multi_address = Multiaddr(f'/ip4/{HOST}/tcp/{PORT}')
 class IPFSError(Exception):
     """Custom exception for IPFS-related errors."""
     pass
+
+
+class IPFSCache:
+    """A simple cache for IPFS data."""
+
+    def __init__(self):
+        self._cache = {}
+
+    def get(self, cid: str):
+        return self._cache.get(cid)
+
+    def set(self, cid: str, data: Dict):
+        self._cache[cid] = data
+
+
+ipfs_cache = IPFSCache()
+
+
+@contextmanager
+def event_loop():
+    loop = asyncio.new_event_loop()
+    try:
+        yield loop
+    finally:
+        loop.close()
 
 
 async def get(cid: str) -> str:
@@ -70,10 +95,9 @@ async def _get_json(cid: str) -> Dict:
     Returns:
         Dict: The JSON data retrieved from IPFS.
     """
-    global IPFS_CACHE
-
-    if cid in IPFS_CACHE:
-        return IPFS_CACHE[cid]
+    cached_data = ipfs_cache.get(cid)
+    if cached_data:
+        return cached_data
 
     try:
         data = await get(cid=cid)
@@ -85,7 +109,7 @@ async def _get_json(cid: str) -> Dict:
     except Exception as e:
         raise IPFSError(f'Failed to parse json data from IPFS hash {cid}: {e}')
 
-    IPFS_CACHE[cid] = data
+    ipfs_cache.set(cid, json_data)
     return json_data
 
 
@@ -98,9 +122,8 @@ def add_json(data: Dict) -> str:
     Returns:
         str: The Content Identifier (CID) of the added JSON data.
     """
-    loop = asyncio.new_event_loop()
-    cid = loop.run_until_complete(_add_json(data=data))
-    loop.close()
+    with event_loop() as loop:
+        cid = loop.run_until_complete(_add_json(data=data))
     return cid
 
 
@@ -113,7 +136,6 @@ def get_json(cid: str) -> Dict:
     Returns:
         Dict: The JSON data retrieved from IPFS.
     """
-    loop = asyncio.new_event_loop()
-    json_data = loop.run_until_complete(_get_json(cid=cid))
-    loop.close()
+    with event_loop() as loop:
+        json_data = loop.run_until_complete(_get_json(cid=cid))
     return json_data
