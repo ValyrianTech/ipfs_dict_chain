0d52d9ce4548b473095a9b9a1b22ddbd5cadbf1c
Wouter Glorieux <wouter@valyrian.tech>
2023-05-03 23:02:52 +0200

    fix after changes

--- IPFSDictChain.py
+++ IPFSDictChain.py
@@ -1,6 +1,6 @@
-from typing import Optional, Dict, Any, Coroutine
+from typing import Optional, Dict, Any
 
-from ipfs_dict_chain import add_json
+from IPFS import add_json
 from IPFSDict import IPFSDict
 
 
@@ -10,26 +10,26 @@ class IPFSDictChain(IPFSDict):
 
         super(IPFSDictChain, self).__init__(cid=cid)
 
-    def save(self) -> Coroutine[Any, Any, str]:
+    def save(self) -> str:
         self.previous_cid = self._cid
-        self._cid = add_json(data=self.get())
+        self._cid = add_json(data=self.items())
         return self._cid
 
     def changes(self) -> Dict[str, Dict[str, Any]]:
         changes = {}
         if self.previous_cid is not None:
-            old_data = IPFSDictChain(cid=self.previous_cid).get()
+            old_data = IPFSDictChain(cid=self.previous_cid).items()
 
             for key in old_data:
                 if old_data[key] != self.__getattribute__(key):
                     changes[key] = {'old': old_data[key], 'new': self.__getattribute__(key)}
 
-            for key in self.get():
+            for key in self.items():
                 if key not in old_data:
                     changes[key] = {'new': self.__getattribute__(key)}
 
         else:
-            for key in self.get():
+            for key in self.items():
                 changes[key] = {'new': self.__getattribute__(key)}
 
         return changes
