7826f48382f5378de09460a7bd9962d8f89fdfc5
Wouter Glorieux <wouter@valyrian.tech>
2023-05-03 22:19:32 +0200

    added docstrings

--- IPFSDict.py
+++ IPFSDict.py
@@ -1,10 +1,16 @@
-from typing import Optional, Dict, Any, Coroutine
+from typing import Optional, Dict, Any
 
-from ipfs_dict_chain import IPFSError, add_json, get_json
+from IPFS import IPFSError, add_json, get_json
 from CID import CID
 
 
 class IPFSDict:
+    """A dictionary-like object that stores its data on IPFS.
+
+    :param cid: The IPFS content identifier (CID) of the dictionary data, defaults to None
+    :type cid: Optional[str], optional
+    """
+
     def __init__(self, cid: Optional[str] = None):
         self._cid = CID(cid).__str__() if cid is not None else None
 
@@ -12,16 +18,38 @@ class IPFSDict:
             self.load(cid=self._cid)
 
     def get(self) -> Dict[str, Any]:
+        """Get the dictionary data.
+
+        :return: The dictionary data
+        :rtype: Dict[str, Any]
+        """
         return {key: value for key, value in self.__dict__.items() if key[0] != '_'}
 
     def cid(self) -> str:
+        """Get the IPFS content identifier (CID) of the dictionary data.
+
+        :return: The CID
+        :rtype: str
+        """
         return self._cid
 
-    def save(self) -> Coroutine[Any, Any, str]:
+    def save(self) -> str:
+        """Save the dictionary data to IPFS and update the CID.
+
+        :return: The new CID
+        :rtype: str
+        """
         self._cid = add_json(data=self.get())
         return self._cid
 
     def load(self, cid: str) -> None:
+        """Load the dictionary data from IPFS using the given CID.
+
+        :param cid: The IPFS content identifier (CID) of the dictionary data
+        :type cid: str
+        :raises ValueError: If the CID is not a string
+        :raises IPFSError: If there is an issue retrieving the data from IPFS
+        """
         if not isinstance(cid, str):
             raise ValueError(f'Can not retrieve IPFS data: cid must be a string or unicode, got {type(cid)} instead')
 
