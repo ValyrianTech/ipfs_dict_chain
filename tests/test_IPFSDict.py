a3d7608cc853a099273e99e9ed496060e58b173f
Wouter Glorieux <wouter@valyrian.tech>
2023-05-03 22:47:37 +0200

    unittests

---
+++ tests/test_IPFSDict.py
@@ -0,0 +1,45 @@
+import unittest
+from IPFSDict import IPFSDict
+from IPFS import IPFSError
+
+
+class TestIPFSDict(unittest.TestCase):
+
+    def test_init(self):
+        ipfs_dict = IPFSDict()
+        self.assertIsNone(ipfs_dict.cid())
+
+        ipfs_dict = IPFSDict(cid="QmV5mPAcGoqegJnzFheED2pnef96633jSjimR2SSgu7ZV5")
+        self.assertEqual(ipfs_dict.cid(), "/ipfs/QmV5mPAcGoqegJnzFheED2pnef96633jSjimR2SSgu7ZV5")
+
+    def test_get(self):
+        ipfs_dict = IPFSDict()
+        ipfs_dict.key = "value"
+        self.assertEqual(ipfs_dict.get(), {"key": "value"})
+
+    def test_cid(self):
+        ipfs_dict = IPFSDict(cid="QmV5mPAcGoqegJnzFheED2pnef96633jSjimR2SSgu7ZV5")
+        self.assertEqual(ipfs_dict.cid(), "/ipfs/QmV5mPAcGoqegJnzFheED2pnef96633jSjimR2SSgu7ZV5")
+
+    def test_save(self):
+        ipfs_dict = IPFSDict()
+        ipfs_dict.key = "value"
+        cid = ipfs_dict.save()
+        self.assertIsNotNone(cid)
+        self.assertNotEqual(cid, "")
+
+    def test_load(self):
+        ipfs_dict = IPFSDict()
+
+        with self.assertRaises(ValueError):
+            ipfs_dict.load(cid=123)
+
+        with self.assertRaises(IPFSError):
+            ipfs_dict.load(cid="this is invalid CID")
+
+        ipfs_dict.load(cid="QmV5mPAcGoqegJnzFheED2pnef96633jSjimR2SSgu7ZV5")
+        self.assertEqual(ipfs_dict.cid(), "/ipfs/QmV5mPAcGoqegJnzFheED2pnef96633jSjimR2SSgu7ZV5")
+
+
+if __name__ == '__main__':
+    unittest.main()
